9f6b8a37973fdb750985d69662ee7cef8ba19030
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-10-27 02:58:18 -0500

    fixed login button style

--- components/Login.jsx
+++ components/Login.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
   return (
     <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-blue-100 via-blue-200 to-blue-300">
       {/* Left Section */}
-      <div className="flex flex-col w-full lg:w-1/2 xl:w-1/3 bg-white rounded-lg shadow-xl justify-center items-center p-8 md:p-10 min-h-screen lg:min-h-0 mx-auto lg:mx-0">
+      <div className="flex flex-col w-full lg:w-1/2 xl:w-1/3 bg-gray-900 rounded-lg shadow-xl justify-center items-center p-8 md:p-10 min-h-screen lg:min-h-0 mx-auto lg:mx-0">
         <div className="flex items-center space-x-3 md:space-x-4 mb-6 md:mb-8">
           <img
             src="/images/logo3.png"
@@ -55,7 +55,7 @@ const LoginPage = () => {
             <button
               key={provider.id}
               onClick={() => signIn(provider.id)}
-              className="flex items-center justify-center bg-gray-50 border border-gray-300 px-8 md:px-10 py-3 md:py-4 rounded-full shadow-lg hover:bg-gray-200 hover:shadow-xl transition-all duration-200 ease-in-out text-gray-900 w-full max-w-xs text-lg md:text-xl"
+              className="flex items-center text-white justify-center bg-grey-700 border border-blue-700 px-8 md:px-10 py-3 md:py-4 rounded-full shadow-lg hover:bg-gray-800 hover:shadow-xl transition-all duration-200 ease-in-out text-gray-900 w-full max-w-xs text-lg md:text-xl"
             >
               <FcGoogle className="mr-3" size={24} />
               <span>Sign in with Google</span>
