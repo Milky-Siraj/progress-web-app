e20b16a6228bf995b7d796306e50f48d3c1046e6
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-10-29 12:11:25 -0500

    middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,13 @@
+// middleware.js or middleware.ts in the root directory
 export { default } from "next-auth/middleware";
+
 export const config = {
   matcher: [
-    "/pages/hometasks",
-    "/pages/team",
+    "/pages/hometasks/:id*",
+    "/pages/team/:id*",
     "/create-team",
     "/pages/profile",
     "/pages/notification",
-    "/pages/bug",
+    "/pages/bug/:id*",
   ],
 };
