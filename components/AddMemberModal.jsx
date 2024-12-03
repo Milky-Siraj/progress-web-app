a878f167cc5247b05a309e166c4f842c82ff526f
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2024-12-03 12:43:02 -0500

    added closemodal for addMembersModal

--- components/AddMemberModal.jsx
+++ components/AddMemberModal.jsx
@@ -138,6 +138,9 @@ const AddMemberModal = ({ closeModal, projectId }) => {
             <button
               type="button"
               className="bg-red-500 text-white px-4 py-2 rounded-lg mr-2"
+              onClick={() => {
+                closeModal();
+              }}
             >
               Cancel
             </button>
